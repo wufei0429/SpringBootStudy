3e768ffbaceb7c5d24a7590480b1273a20e7e18d
wufei0429 <2453804076@qq.com>
2019-03-18 09:38:57 +0800

    spring  Boot first

---
+++ spring-Boot-Study/quickstart/src/main/java/com/springboot/quickstart/QuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//Springboot 的启动主类
+@SpringBootApplication
+public class QuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickstartApplication.class, args);
+    }
+
+}
