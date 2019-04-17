281999f02b7d0d89756a39e3f45b6fd0b236b5e7
wufei0429 <2453804076@qq.com>
2019-04-17 11:18:58 +0800

    websocket

--- spring-Boot-Study/quickstart/src/main/java/com/springboot/quickstart/QuickstartApplication.java
+++ spring-Boot-Study/quickstart/src/main/java/com/springboot/quickstart/QuickstartApplication.java
@@ -1,9 +1,13 @@
 package com.springboot.quickstart;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //Springboot 的启动主类
 @SpringBootApplication
+//@MapperScan("com.springboot.mybatis.mapper")
+@EnableSwagger2Doc
+
 public class QuickstartApplication {
 
     public static void main(String[] args) {
